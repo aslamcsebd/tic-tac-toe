952751969362748b78a4f3ce6ca3b6dd9688a19e
aslamcsebd <aslamcsebd@gmail.com>
2022-10-05 23:23:49 +0600

    Front page design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,11 +41,15 @@
             }
 
             .content {
+                background-color: whitesmoke;
                 text-align: center;
+                padding: 10px;
+                border: 1px dashed #000;
+                border-radius: 5px;
             }
 
             .title {
-                font-size: 84px;
+                font-size: 40px;
             }
 
             .links > a {
@@ -57,10 +61,18 @@
                 text-decoration: none;
                 text-transform: uppercase;
             }
-
-            .m-b-md {
-                margin-bottom: 30px;
+            .content a{
+                font-size: 40px;
+                text-decoration: none;
+                border: 1px dashed #000;
+                border-radius: 5px;
+                padding: 0px 5px;
             }
+            .content a:hover{
+                background-color: gainsboro;
+                color: #000;
+            }
+            
         </style>
     </head>
     <body>
@@ -81,19 +93,18 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                    <div>Play tic-tac-toe game<br>First time Register & Login</div> <br>
+                    @if (Route::has('login'))
+                        @auth
+                            <a href="{{ url('/home') }}">Home</a>
+                        @else
+                            <a href="{{ route('login') }}">Login</a>    
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}">Register</a>
+                            @endif
+                        @endauth
+                    @endif
+                </div>                
             </div>
         </div>
     </body>
